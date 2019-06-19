c23ddb1c76353c09af4b7c45ee6a796384409ff0
Tomoki <tech54tu@gmail.com>
2019-06-19 12:31:04 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.all
     respond_to do |format|
       format.html
-      format.json { @new_message = Message.where('id > ?', params[:message][:id])}
+      format.json { @new_message = Message.where('id > ?', params[:id])}
     end
   end
 end
